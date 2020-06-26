82f7411cfe96b136f199594b96f34d84d88be03a
Yash <yashkaushikbits19@gmail.com>
2020-06-26 17:42:39 +0530

    Schema Added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Form from "@rjsf/core";
+import "./index.css";
+import App from "./App";
+import Navigation from "reactstrap-json-nav";
+import * as serviceWorker from "./serviceWorker";
+
+var schema = require("./schema.json");
+var uiSchema = require("./uiSchema.json");
+
+let json = [
+  { name: "Nav List", url: "/components/" },
+  { name: "Nav Item 1", url: "/components/" },
+  { name: "Nav Item 2", url: "https://github.com/reactstrap/reactstrap" },
+  { name: "Nav Item 3", url: "https://github.com/reactstrap/reactstrap" },
+];
+
+const log = (type) => console.log.bind(console, type);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Navigation json={json} bg="light" navbar-expand="lg" navbar-expand-lg />
+    <Form
+      schema={schema}
+      uiSchema={uiSchema}
+      onChange={log("changed")}
+      onSubmit={log("submitted")}
+      onError={log("errors")}
+    />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
