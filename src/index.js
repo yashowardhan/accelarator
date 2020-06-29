139465b60fdef36c6656954bbf32dce311649d09
amnahkhatun <amna.khatun1605@gmail.com>
2020-06-29 17:44:04 +0530

    implemented basic json to table conversion

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Form from "@rjsf/core";
 import "./index.css";
 import App from "./App";
 import Navigation from "reactstrap-json-nav";
+import { JsonToTable } from "react-json-to-table";
 import * as serviceWorker from "./serviceWorker";
 
 var schema = require("./schema.json");
@@ -13,15 +14,22 @@ let json = [
   { name: "Nav List", url: "/components/" },
   { name: "Nav Item 1", url: "/components/" },
   { name: "Nav Item 2", url: "https://github.com/reactstrap/reactstrap" },
-  { name: "Nav Item 3", url: "https://github.com/reactstrap/reactstrap" },
+  { name: "Nav Item 3", url: "https://github.com/reactstrap/reactstrap" }
 ];
 
-const log = (type) => console.log.bind(console, type);
+let items = [
+  { name: "Louise", age: 27, color: "red" },
+  { name: "Margaret", age: 15, color: "blue" },
+  { name: "Lisa", age: 34, color: "yellow" }
+];
+
+const log = type => console.log.bind(console, type);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Navigation json={json} bg="light" navbar-expand="lg" navbar-expand-lg />
+    <JsonToTable json={items} />{" "}
     <Form
       schema={schema}
       uiSchema={uiSchema}
